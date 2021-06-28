bcbc90160ad5c9b421f742f5a36e5be834a5163d
Eric Rönnhult <zedpentium@gmail.com>
2021-06-28 14:04:08 +0200

    Done step 3. Commiting

---
+++ ConApp_ER_ToDo.Tests/PersonTests.cs
@@ -0,0 +1,50 @@
+using ConApp_ER_ToDo.Model;
+using System;
+using Xunit;
+
+namespace ConApp_ER_ToDo.Tests
+{
+    public class PersonTests
+    {
+        
+
+        [Fact]
+        public void PersonClassTest()
+        {
+            //Arrange
+            string firstName = "Kent";
+            string lastName = "Svensson";
+            string firstName2 = "Test";
+            string lastName2 = "Testsson";
+
+            //Act
+            Person person1 = new Person(firstName, lastName);
+            Person person2 = new Person(firstName2, lastName2);
+
+            //Assert
+            Assert.NotEqual(person1.personIdnr, person2.personIdnr);
+            Assert.Equal(person2, person2);
+            Assert.NotEqual(person1, person2);
+
+            //Console.WriteLine($"Output: {person1} { person2}" );
+        }
+
+
+        [Fact]
+        public void Testsomething()
+        {
+
+            //arrange
+
+
+
+            //act
+
+
+
+            //assert
+        }
+
+
+    }
+}
---
+++ ConApp_ER_ToDo/Model/Person.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConApp_ER_ToDo.Model
+{
+    public class Person
+    {
+        static int idCounter;
+
+        public int personIdnr;
+
+        readonly int personId;
+
+        string firstName;
+        string lastName;
+
+
+        //, int personId
+        //  PersonId = personId
+        public Person(string firstName, string lastName)
+        {
+            personIdnr = ++idCounter;
+            FirstName = firstName;
+            LastName = lastName;
+
+        }
+
+        //// A read-write instance property:
+        //public string Name
+        //{
+        //    get => _name;
+        //    set => _name = value;
+        //}
+
+        //// A read-only static property:
+        //public static int Counter => _counter;
+
+
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Empty or only whitespace is not allowed.");
+                }
+
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Empty or only whitespace is not allowed.");
+                }
+
+                lastName = value;
+            }
+        }
+        public int PersonId
+        {
+            get { return personIdnr; }
+            set
+            {
+                //if (string.IsNullOrWhiteSpace(value))
+                //{
+                //    throw new ArgumentException("Empty or only whitespace is not allowed.");
+                //}
+
+                personIdnr = value;
+            }
+        }
+
+
+
+    }
+}
