49230ab3c50631cc27439d4561fa08e89702ee86
Eric Rönnhult <zedpentium@gmail.com>
2021-06-28 11:49:13 +0200

    Add project files.

---
+++ ConApp_ER_ToDo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConApp_ER_ToDo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
