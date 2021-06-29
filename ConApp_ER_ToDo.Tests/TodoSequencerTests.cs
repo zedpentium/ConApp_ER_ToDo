38cdad1b33296b8307b867628f38bbf32f741ec1
Eric Rönnhult <zedpentium@gmail.com>
2021-06-29 13:23:25 +0200

    Step 7 Done

---
+++ ConApp_ER_ToDo.Tests/TodoSequencerTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using ConApp_ER_ToDo.Data;
+
+namespace ConApp_ER_ToDo.Data.Tests
+{
+    public class TodoSequencerTests
+    {
+
+
+        [Fact]
+        public void TodoSequencerClassTest()
+        {
+            //Arrange
+            TodoSequencer.NextToDoId(7);
+
+            //Act
+
+            //Assert
+            Assert.Equal(8, TodoSequencer.PersonId);
+
+        }
+
+        [Fact]
+        public void ToDoIdTest()
+        {
+            //Arrange
+            TodoSequencer.NextToDoId(3);
+
+            //Act
+            TodoSequencer.ToDoReset();
+
+
+            //Assert
+
+            
+            Assert.Equal(0, TodoSequencer.PersonId);
+            
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
---
+++ ConApp_ER_ToDo/Data/TodoSequencer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConApp_ER_ToDo.Data
+{
+    public class TodoSequencer
+    {
+
+        private static int personId;
+
+        public TodoSequencer(int personId)
+        {
+            PersonId = personId;
+        }
+
+        public static int PersonId
+            {
+            get
+                {
+                    return personId;
+                }
+            set
+                {
+                    personId = value;
+                }
+            }
+
+
+
+        public static int NextToDoId(int toDoIdNext)
+            {
+
+            PersonId = ++toDoIdNext;
+
+            return PersonId;
+            }
+
+
+
+        public static void ToDoReset()
+        {
+            PersonId = 0;
+        }
+
+
+
+    }
+}
